2aa5525d258d294e9d6b1883a5a18cca4554dcde
ZiyanZeng <2579985009@qq.com>
2026-06-01 16:53:57 +0800

    This is my second commit message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changeMessage() {
     const message = document.getElementById("message");
-    message.textContent = "Hello World to AI in Software Engineering 🚀";
+    message.textContent = "Hello World to AI in Software Engineering 🚀！！！";
 }
